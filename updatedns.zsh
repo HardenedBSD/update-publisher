06e2502adc4479118c36219baa284bec417d844f
Shawn Webb <shawn.webb@hardenedbsd.org>
2015-12-31 20:28:03 -0500

    Add script to update DNS

---
+++ updatedns.zsh
@@ -0,0 +1,66 @@
+#!/usr/local/bin/zsh
+
+name=${1}
+recordname=${2}
+buildver=${3}
+recordid=""
+zoneid=""
+apiurl="https://api.cloudflare.com/client/v4"
+tmpfile=$(mktemp)
+. $(dirname $(realpath ${0}))/api.key
+
+function get_zoneid() {
+	curl -s -X GET "${apiurl}/zones?name=${name}" \
+		-H "X-Auth-Email: shawn.webb@hardenedbsd.org" \
+		-H "X-Auth-Key: ${apikey}" \
+		-H "Content-Type: application/json" \
+		-o ${tmpfile}
+
+	zoneid=$(jq -r '.result[0].id' ${tmpfile})
+}
+
+function get_recordid() {
+	curl -s -X GET "${apiurl}/zones/${zoneid}/dns_records?name=${recordname}" \
+		-H "X-Auth-Email: shawn.webb@hardenedbsd.org" \
+		-H "X-Auth-Key: ${apikey}" \
+		-H "Content-Type: application/json" \
+		-o ${tmpfile}
+
+	recordid=$(jq -r '.result[0].id' ${tmpfile})
+}
+
+function generate_new_record_data() {
+cat<<EOF > ${tmpfile}
+{
+	"id": "${recordid}",
+	"type": "TXT",
+	"name": "${recordname}",
+	"content": "${buildver}",
+	"proxiable": false,
+	"proxied": false,
+	"ttl": 3600,
+	"locked": false,
+	"zoneid": "${zoneid}",
+	"zone_name": "${name}",
+	"data": {}
+}
+EOF
+}
+
+function update_record() {
+	echo "[*] Updating DNS to reflect version: ${buildver}" >&2
+
+	generate_new_record_data
+
+	curl -s -X PUT "${apiurl}/zones/${zoneid}/dns_records/${recordid}" \
+		-H "X-Auth-Email: shawn.webb@hardenedbsd.org" \
+		-H "X-Auth-Key: ${apikey}" \
+		-H "Content-Type: application/json" \
+		--data "@${tmpfile}" \
+		-o ${tmpfile}
+
+}
+
+get_zoneid
+get_recordid
+update_record
